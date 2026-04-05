f08df4fade21c0aa5ed18a200908d46a15610994
Dave Vera <dave@davevera.com>
2026-04-05 11:53:57 +0200

    chore: sync database images locally and update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'luablfcmhzykjnxmtlqh.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
